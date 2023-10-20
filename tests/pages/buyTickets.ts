e80c4f6c3d264b35b362950452800899a9fa22d6
Nick Norman <nicknorm007@gmail.com>
2023-10-20 12:33:34 -0400

    add verify start

--- tests/pages/buyTickets.ts
+++ tests/pages/buyTickets.ts
@@ -8,6 +8,7 @@ export const buyTickets = {
   date_from_input: "#datepicker-first",
   date_to_input: "#datepicker-second",
   submit_button: "input[type=submit]",
+  cancel_after_submit_button: "input[id=exitButton]",
   enterFromTo: async (page, from: string, to: string) => {
     await page.locator(buyTickets.from_input).fill(from);
     await page.locator(buyTickets.to_input).fill(to);
@@ -15,8 +16,12 @@ export const buyTickets = {
   enterFromToDates: async (page, from: string, to: string) => {
     await page.locator(buyTickets.date_from_input).fill(from);
     await page.locator(buyTickets.date_to_input).fill(to);
+    await page.locator(buyTickets.date_to_input).press("Enter");
   },
   submit: async (page) => {
     await page.locator(buyTickets.submit_button).click();
+  },
+  cancelAfterSubmit: async (page) => {
+    await page.locator(buyTickets.cancel_after_submit_button).click();
   },
 } as const;
--- tests/tickets/buyTickets.spec.ts
+++ tests/tickets/buyTickets.spec.ts
@@ -11,17 +11,20 @@ test("Verify buy tickets page title", async ({ page }) => {
   expect(pageTitle).toBe(buyTickets.title);
 });
 
-test("Enter sample train times and destinations", async ({ page }) => {
-  await buyTickets.enterFromTo(page, "Lagos", "Porto - Campanha");
-  await page.waitForTimeout(3000); // remove these
-
+test("Enter sample train times and destinations and verify cancel saves data", async ({
+  page,
+}) => {
+  let from_location = "Lagos";
+  let to_location = "Porto - Campanha";
+  await buyTickets.enterFromTo(page, from_location, to_location);
   const from_date = dateUtils.getDateStringInFutureByNumberofDays(3);
   const to_date = dateUtils.getDateStringInFutureByNumberofDays(5);
   await buyTickets.enterFromToDates(page, from_date, to_date);
-
-  await page.waitForTimeout(3000); // remove these
-
   await buyTickets.submit(page);
+  await buyTickets.cancelAfterSubmit(page);
 
-  await page.waitForTimeout(3000); // remove these
+  page.setDefaultTimeout(3000);
+
+  await expect(page.locator(buyTickets.from_input)).toHaveValue(from_location);
+  await expect(page.locator(buyTickets.to_input)).toHaveValue(to_location);
 });
