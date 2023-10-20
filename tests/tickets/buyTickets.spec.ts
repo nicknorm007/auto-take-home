7c5bb1f8841f24bb08fc0e793bef114c81222e10
Nick Norman <nicknorm007@gmail.com>
2023-10-20 14:07:24 -0400

    passing test

--- tests/tickets/buyTickets.spec.ts
+++ tests/tickets/buyTickets.spec.ts
@@ -11,20 +11,30 @@ test("Verify buy tickets page title", async ({ page }) => {
   expect(pageTitle).toBe(buyTickets.title);
 });
 
-test("Enter sample train times and destinations and verify cancel saves data", async ({
-  page,
-}) => {
-  let from_location = "Lagos";
-  let to_location = "Porto - Campanha";
-  await buyTickets.enterFromTo(page, from_location, to_location);
-  const from_date = dateUtils.getDateStringInFutureByNumberofDays(3);
-  const to_date = dateUtils.getDateStringInFutureByNumberofDays(5);
-  await buyTickets.enterFromToDates(page, from_date, to_date);
-  await buyTickets.submit(page);
-  await buyTickets.cancelAfterSubmit(page);
-
-  page.setDefaultTimeout(3000);
-
-  await expect(page.locator(buyTickets.from_input)).toHaveValue(from_location);
-  await expect(page.locator(buyTickets.to_input)).toHaveValue(to_location);
-});
+const destinations = [
+  { from: "Lagos", to: "Porto - Campanha", start_in_days: 3, end_in_days: 5 },
+];
+for (const destination of destinations) {
+  test(`Enter sample train times and destinations ${destination} and verify cancel preserves form data`, async ({
+    page,
+  }) => {
+    await buyTickets.enterFromTo(page, destination.from, destination.to);
+    const from_date = dateUtils.getDateStringInFutureByNumberofDays(
+      destination.start_in_days
+    );
+    const to_date = dateUtils.getDateStringInFutureByNumberofDays(
+      destination.end_in_days
+    );
+    await buyTickets.enterFromToDates(page, from_date, to_date);
+    await buyTickets.submit(page);
+    await buyTickets.cancelAfterSubmit(page);
+    await expect(page.locator(buyTickets.from_input)).toHaveValue(
+      destination.from
+    );
+    await expect(page.locator(buyTickets.to_input)).toHaveValue(destination.to);
+    await expect(page.locator(buyTickets.date_from_input)).toHaveValue(
+      from_date
+    );
+    await expect(page.locator(buyTickets.date_to_input)).toHaveValue(to_date);
+  });
+}
